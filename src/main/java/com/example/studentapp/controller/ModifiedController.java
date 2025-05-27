29d08326d02f665b3bac7db629e2b85454e9fdd9
hari <complex7989@gmail.com>
2025-05-27 19:23:05 +0530

    modified text

--- src/main/java/com/example/studentapp/controller/ModifiedController.java
+++ src/main/java/com/example/studentapp/controller/ModifiedController.java
@@ -1,4 +1,4 @@
-package com.example.studentapp.controller;
+ackage com.example.studentapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,6 @@ public class StudentController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from StudentController!";
+        return "Hello Everyone !";
     }
 }
